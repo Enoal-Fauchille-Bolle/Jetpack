8c25f19c14d75622a88ac4d12a752afdf44b8e04
Tom Feldkamp <tom.feldkamp@epitech.eu>
2025-04-18 16:09:35 +0200

    feat(player): ✨ | implement Player class with texture loading, position management, and drawing logic

--- client/src/elements/Player.cpp
+++ client/src/elements/Player.cpp
@@ -7,16 +7,34 @@
 
 #include "Player.hpp"
 
-/**
- * @brief Construct a new Player::Player object
- */
 Player::Player()
+{
+    sf::Texture texture;
+    if (!texture.loadFromFile("assets/jetpack_player.png")) {
+        std::cerr << "Error loading texture" << std::endl;
+        return;
+    }
+    _sprite.setTexture(texture);
+    _sprite.setPosition(0, 0);
+    _sprite.setScale(0.1f, 0.1f);
+    _sprite.setOrigin(_sprite.getGlobalBounds().width / 2,
+        _sprite.getGlobalBounds().height / 2);
+    _sprite.setColor(sf::Color(255, 255, 255, 255));
+}
+
+Player::~Player()
 {
 }
 
-/**
- * @brief Destroy the Player::Player object
- */
-Player::~Player()
+void Player::NewPosition(float x, float y)
 {
+    sf::Vector2f currentPos = _sprite.getPosition();
+    sf::Vector2f newPos(currentPos.x + x, currentPos.y + y);
+
+    _sprite.setPosition(newPos);
+}
+
+void Player::Draw(sf::RenderWindow &window)
+{
+    window.draw(_sprite);
 }
--- client/src/elements/Player.hpp
+++ client/src/elements/Player.hpp
@@ -8,13 +8,24 @@
 #ifndef PLAYER_HPP_
 #define PLAYER_HPP_
 
+    #include <SFML/Graphics.hpp>
+    #include <iostream>
+    #include <string>
+
 class Player {
     public:
         Player();
         ~Player();
 
+        void NewPosition(float x, float y);
+        void Draw(sf::RenderWindow &window);
+
     protected:
     private:
+        sf::Sprite _sprite;
+        sf::Texture _texture;
+        sf::Vector2f _position;
+
 };
 
 #endif /* !PLAYER_HPP_ */
